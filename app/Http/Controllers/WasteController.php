377b52d5868a485d8c4c859fc2d7e98d46f66065
Franklin Shera <fshera96@gmail.com>
2021-11-29 17:58:28 +0300

    WASTE

---
+++ app/Http/Controllers/WasteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WasteController extends Controller
+{
+    public function store(){
+        return "Waste Store";
+    }
+
+    public function update(){
+        return "Waste Update";
+    }
+
+    public function destroy(){
+        return "Waste Destroy";
+    }
+
+
+
+}
---
+++ database/migrations/2021_11_29_144212_create_wastes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wastes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->double('cost' ,3,2)->default(0.00);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wastes');
+    }
+}
