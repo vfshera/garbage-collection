4f910b7f544e5ec9fbbba55a9acb811d0cf524d8
Franklin Shera <fshera96@gmail.com>
2021-12-06 08:45:02 +0300

    USER VIEW MODAL

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -20,7 +20,7 @@
 
             <div class="users-list">
                 @foreach($users as $key => $normalUser)
-                <div class="user">
+                <div class="user" onclick="view({{ $normalUser }},'#viewModal')">
                     <div class="name">
                         {{ $key + 1 }}. {{ $normalUser->name }}
                     </div>
@@ -42,4 +42,128 @@
             {{ $users->links() }}
         </div>
     </div>
+
+
+    <!-- PAGE MODALS -->
+    <x-slot name="modals">
+        <div id="viewModal" class="modal">
+
+            <div id="userInfoModal" class="modal-content">
+                <header>
+                    <h2 id="viewHeadTitle"></h2>
+                    <span id="closeIcon" onclick="closeModal('#viewModal')" class="cursor-pointer">&times;</span>
+                </header>
+
+                <div id="infoContainer" class="info">
+
+                    <div class="info-item">
+                        <p id="viewLabel">Email</p>
+                        <p id="viewEmail"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Phone</p>
+                        <p id="viewPhone"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Postal</p>
+                        <p id="viewPostal"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Address</p>
+                        <p id="viewAddress"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">County</p>
+                        <p id="viewCounty"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Constituency</p>
+                        <p id="viewConstituency"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Village</p>
+                        <p id="viewVillage"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Ward</p>
+                        <p id="viewWard"></p>
+                    </div>
+
+
+                    <div class="info-item">
+                        <p id="viewLabel">Town</p>
+                        <p id="viewTown"></p>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </x-slot>
+
+
+
+    <x-slot name="pagescripts">
+        <script>
+        const closeModal = (modal) => {
+
+            document.querySelector(modal).style.display = "none";
+        }
+
+
+        const openModal = (modal) => {
+            document.querySelector(modal).style.display = "block";
+        }
+
+        //VIEW MODAL
+        var viewHeadTitle = document.querySelector("#viewHeadTitle");
+        var infoCont = document.querySelector("#infoContainer");
+
+        const view = (user, modal) => {
+            openModal(modal);
+
+            console.log(user);
+
+
+            viewHeadTitle.innerText = user.name;
+
+            document.querySelector("#viewEmail").innerText = user.email;
+
+            document.querySelector("#viewPhone").innerText = user.phone;
+
+            document.querySelector("#viewPostal").innerText = user.postal_code;
+
+            document.querySelector("#viewAddress").innerText = user.address;
+
+            document.querySelector("#viewCounty").innerText = user.county;
+
+            document.querySelector("#viewConstituency").innerText = user.constituency;
+
+            document.querySelector("#viewVillage").innerText = user.village;
+
+            document.querySelector("#viewWard").innerText = user.ward;
+
+            document.querySelector("#viewTown").innerText = user.town;
+
+        }
+        </script>
+    </x-slot>
+
 </x-app-layout>
