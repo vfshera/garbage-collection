e1a9ade3422bd5219c5dc66ddf6bb4f973e509d7
Franklin Shera <fshera96@gmail.com>
2021-12-18 13:52:44 +0300

    TRANSACTION SEEDER LOGIC

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Str;
 class TransactionSeeder extends Seeder
 {
     /**
@@ -13,6 +15,20 @@ class TransactionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $orders = Order::paid()->with('user')->get();
+
+
+        foreach($orders as $order){
+
+            Transaction::create(
+                [
+                    'order_id' => $order->id,             
+                    'MerchantRequestID' => Str::random(28),
+                    'CheckoutRequestID' => Str::random(18),
+                    'Amount' => $order->cost
+                ]
+            );
+        }
+
     }
 }
